f9cfd8eca3c21b843e578f7651698fc0f1c49b97
wamu <101846564+tkzero789@users.noreply.github.com>
2024-09-20 20:47:15 -0400

    fix: income tooltip for mobile

--- piton-app/app/(finance)/income/_components/IncomeCustomTooltip.tsx
+++ piton-app/app/(finance)/income/_components/IncomeCustomTooltip.tsx
@@ -1,3 +1,4 @@
+import useWindowSize from "@/hooks/useWindowSize";
 import FormatNumber from "@/utils/formatNumber";
 import React from "react";
 
@@ -8,12 +9,23 @@ type Props = {
 };
 
 export default function IncomeCustomTooltip({ active, payload, label }: Props) {
+  const { width } = useWindowSize();
+
+  const formatLabel = (label: string) => {
+    const monthIndex = parseInt(label, 10);
+    return new Date(0, monthIndex).toLocaleDateString("default", {
+      month: "short",
+    });
+  };
+
+  const displayLabel = (width ?? 0) < 768 ? formatLabel(label ?? "") : label;
+
   if (active && payload && payload.length) {
     return (
       <div className="rounded-lg border bg-white p-2 shadow-md">
         <div className="flex items-center gap-2">
           <div className="block h-3 w-3 rounded-sm bg-[#2a9d90]"></div>
-          <p className="font-bold">{`${label}`}</p>
+          <p className="font-bold">{`${displayLabel}`}</p>
         </div>
         <p>
           Income:{" "}
