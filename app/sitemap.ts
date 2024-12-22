cf85e26df3275e9c0b61a7f0a786f2652b42ae68
Thinh Tran <thinhtran0061@gmail.com>
2024-12-22 11:37:52 -0500

    feat: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
+    {
+      url: "https://dimebudget.com/privacy-policy",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://dimebudget.com/terms-of-service",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
   ];
 }
